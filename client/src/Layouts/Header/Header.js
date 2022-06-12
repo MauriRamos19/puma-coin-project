2b35bfb245bf73c6c6e2ced609e6ba4a53b794f3
Hespinoza <henryespinoza13@gmail.com>
2022-06-12 03:43:07 -0600

    creating the header navbar

--- client/src/Layouts/Header/Header.js
+++ client/src/Layouts/Header/Header.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logoImg from '../../Assets/images/pumaCoinLogo.png'
+import Button from '../../Components/Button/Button'
+import './Header.css'
 
 const Header = (props) => {
     return (
-        <div>
-            Header
-            <Link to="/">home</Link>
-            <Link to="/trade">trade</Link>
-            <Link to="/support">support</Link>
+        <div className='Header'>
+            <Link 
+                to='/'
+                className='Header__logo'
+            >
+                <img className='Header__logo__img' src={logoImg} />
+                <h2 className='Header__logo__name'>PumaCoin</h2>
+            </Link>
+            <div className='Header__navigation__wrapper'>
+                <nav className='Header__navigation'>
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/trade'>Trade</Link></li>
+                        <li><Link to='/support'>Support</Link></li>
+                    </ul>
+                </nav>
+                <div className='Header__account'>
+                    <Link
+                        to='/login'
+                        className='Header__account__login'
+                    >
+                        Login
+                    </Link>
+                    <Link
+                        to='/register'
+                        className='Header__account__register'
+                    >
+                        <Button>
+                            Register
+
+                        </Button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
