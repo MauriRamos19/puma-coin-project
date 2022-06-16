a0b8a3c84acf57b764212d30ea196d690de00798
EnriqueDll <grift210@gmail.com>
2022-06-15 18:29:59 -0600

    imagenes en landing

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -2,12 +2,18 @@ import React from "react";
 import Button from "../../Components/Button/Button";
 import InputWithLabel from "../../Components/InputWithLabel/InputWithLabel";
 import WrapperDirection from "../../Components/WrapperDirection/WrapperDirection";
+import moneda from '../../Assets/images/moneda.png'
+import logoIs from '../../Assets/images/logoIs.png'
+import tarjeta from '../../Assets/images/tarjetaUnah.png'
+
+
 import "./Home.css";
 
 const Home = (props) => {
 	return (
 		<div className="Home">
 			<div className="Home__first_column_wrapper">
+
 				<div className="Home__title_column_size">
 					<h1>
 						Crypto confiable y seguro!
@@ -23,6 +29,15 @@ const Home = (props) => {
 				<div className="Home__first_column_button">
 					<Button className="Home__first_column_buttonP">Empezar</Button>
 				</div>
+					<div className="Home__image">
+						<img className='Home__img_card' src={tarjeta} />
+					</div>
+					<div className="Home__image">
+						<img className='Home__img_logoIS' src={logoIs} />
+					</div>
+					<div className="Home__image">
+						<img className='Home__img_coin' src={moneda} />
+					</div>
 			</div>
 			<div className="Home__first_column_wrapper">
 				<div className="myDIV">
