6bb840c7dcbbf0b6816255f6b4f6ae6529660529
MauriRamos19 <68834179+MauriRamos19@users.noreply.github.com>
2022-06-15 23:55:17 -0600

    fix: validate fields function and other validations

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { login, register, renewToken } = require("../controllers/auth");
 const { check } = require('express-validator');
 const { isValidPassword, existEmail } = require("../helpers/db-validators");
-const { validateFields, validateFieldsLogin } = require("../middlewares/validate-fields");
+const { validateFields} = require("../middlewares/validate-fields");
 const { validateJWT } = require("../middlewares/validate-JWT");
 const router =  Router();
 
@@ -11,7 +11,7 @@ const router =  Router();
 router.post('/login',[
     check('email', 'Email is required').isEmail(),
     check('password', 'Password is required').not().isEmpty(),
-    validateFieldsLogin
+    validateFields
 ], login);
 
 router.post('/register',[
@@ -19,9 +19,9 @@ router.post('/register',[
     check('email', 'El correo es obligatorio').isEmail(),
     check('password','La contraseña es obligatoria').not().isEmpty(),
     check('password','La contraseña debe tener al menos 6 caracteres').isLength({min:6}),
+    validateFields,
     check('password').custom(isValidPassword),
     check('email','El correo ya esta registrado').custom(existEmail),
-    validateFields
 ],register);
 
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,25 @@
 const { Router } = require("express");
 const { getUser, putUser } = require("../controllers/users");
+const { check } = require('express-validator');
+const { validateFields } = require("../middlewares/validate-fields");
 
 const router =  Router();
 
 
 router.get('/',getUser);
 
-router.put('/:id',putUser);
+router.put('/:id',[
+    check('firstName','firstName is required').isString(),
+    check('lastName','firstName is required').isString(),
+    check('sex','firstName is required').isString(),
+    check('phone','firstName is required').isMobilePhone(),
+    check('address1','firstName is required').isString(),
+    check('address2','firstName is required').isString(),
+    check('city','firstName is required').isString(), 
+    check('country','firstName is required').isString(),   
+    check('department','firstName is required').isString(),
+    check('zipCode','firstName is required').isNumeric(),
+    validateFields     
+],putUser);
 
 module.exports = router;
