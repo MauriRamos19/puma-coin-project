916cefa3a9d5826acb9ea9579a35bf8b7f0aa939
Bryan Martínez <brmartinez@unah.hn>
2022-07-21 23:41:19 -0600

    Update server.js

--- server/models/server.js
+++ server/models/server.js
@@ -27,7 +27,7 @@ class Server {
 
     middlewares() {
         this.app.use(cors({
-            origin: process.env.BASE_URL,
+            origin: "*",
             credentials: true,
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
             allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
