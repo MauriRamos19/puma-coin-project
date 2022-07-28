c96f957281407f21486ce778b11a4189bc31933a
Bryan Martínez <brmartinez@unah.hn>
2022-07-28 02:52:31 -0600

    Update NewCostumer.js

--- client/app/src/Pages/NewCostumer/NewCostumer.js
+++ client/app/src/Pages/NewCostumer/NewCostumer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import cargando from '../../Assets/images/cargando.png'
+import {  ConectWallet } from '../../Layouts/Transactions/conectWallet';
 import "./NewCostumer.css";
 
 const NewCostumer = (props) => {
