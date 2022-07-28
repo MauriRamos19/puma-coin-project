dd205a57074659e5171cedde7847d0d95ae73e26
Bryan Martínez <brmartinez@unah.hn>
2022-07-28 06:54:06 -0600

    Update OldCostumer.js

--- client/app/src/Pages/OldCostumer/OldCostumer.js
+++ client/app/src/Pages/OldCostumer/OldCostumer.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import cargando from '../../Assets/images/cargando.png'
 import {  ConectWallet } from '../../Layouts/Transactions/conectWallet';
 import "./OldCostumer.css";
+import { withCookies } from "react-cookie";
 import { getUser } from '../../services/user';
 
-const OldCostumer = (props) => {
+const OldCostumer = ({cookies}) => {
+    const [name, setName] = useState("")
 
+    useEffect(()=>{
+        const token = cookies.get("x_access_token")
+
+        getUser(token).then(respuesta=>{
+            setName(respuesta.user.name)
+        })
+    },[name])
     return (
         <div>
             <div>
-                <h2>Bienvenido</h2>
+                <h2>Bienvenido {name}</h2>
             </div>
             {ConectWallet()}
         </div>
     )
 }
 
-export default OldCostumer
+export default withCookies(OldCostumer)
