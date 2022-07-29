204c600181e57ef741163e5dbcfa128caedb8302
Bryan Martínez <brmartinez@unah.hn>
2022-07-28 22:15:35 -0600

    cambios en cors

--- server/models/server.js
+++ server/models/server.js
@@ -30,7 +30,10 @@ class Server {
         this.app.use(cors({
             origin: ["http://localhost:3000", "https://pumacoin-finance.web.app", "https://api.devnet.solana.com","https://pumacoin-backend.herokuapp.com"],
             credentials: true,
-            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'],
+            exposedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'],
+            optionsSuccessStatus: 200
         }));
         this.app.use(express.json())
         this.app.use(cookieParser());
