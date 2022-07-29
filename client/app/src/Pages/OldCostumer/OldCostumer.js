8240e60f7b2dfbd11463bcfef2e5c45e1d30015f
Bryan Martínez <brmartinez@unah.hn>
2022-07-28 18:01:20 -0600

    Update OldCostumer.js

--- client/app/src/Pages/OldCostumer/OldCostumer.js
+++ client/app/src/Pages/OldCostumer/OldCostumer.js
@@ -3,6 +3,7 @@ import {  ConectWallet } from '../../Layouts/Transactions/conectWallet';
 import "./OldCostumer.css";
 import { withCookies } from "react-cookie";
 import { getUser } from '../../services/user';
+import model2 from '../../Assets/images/model (2).png'
 
 const OldCostumer = ({cookies}) => {
     const [name, setName] = useState("")
@@ -14,15 +15,25 @@ const OldCostumer = ({cookies}) => {
             setName(respuesta.user.name)
         })
     },[name])
-    return (
-        <div>
+
+    if (!cookies.get("x_access_token"))
+    {
+        return (<div>
+            <h1>Bienvenido a Cliente Recurrente<img  src={model2} /></h1>
+            <h3>Para poder continuar debes acceder a tu cuenta, inicia sesión.</h3>
+            <div><img  src={model2} /></div>
+        </div>)
+    }else{
+        return (
             <div>
-                <h2>Bienvenido {name}, es bueno verte de vuelta.</h2>
-                <h3>Verifica que todo es correcto, proximo viaje hacia ¡LA LUNA!</h3>
+                <div>
+                    <h2>Bienvenido {name}, es bueno verte de vuelta.</h2>
+                    <h3>Verifica que todo es correcto, proximo viaje hacia ¡LA LUNA!</h3>
+                </div>
+                {ConectWallet()}
             </div>
-            {ConectWallet()}
-        </div>
-    )
+        )
+    }
 }
 
 export default withCookies(OldCostumer)
