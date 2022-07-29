257effeb6d11f9ec83d7799d781460c5ada7757a
Bryan Martínez <brmartinez@unah.hn>
2022-07-28 22:27:12 -0600

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 
 const router =  require('express').Router();
-
+const cors = require('cors');
 const { getUser, editUser, deleteAccount } = require('../controllers/user');
 const { validateJWT } = require('../middlewares/validate-JWT');
 
@@ -12,6 +12,12 @@ router.put('/', validateJWT ,editUser);
 
 router.delete('/', validateJWT ,deleteAccount);
 
+router.use(
+    cors({
+        credentials: true,
+        origin: ["http://localhost:3000/", "https://pumacoin-finance.web.app/", "https://api.devnet.solana.com/"]
+    })
+);
 
 
 module.exports = router;
